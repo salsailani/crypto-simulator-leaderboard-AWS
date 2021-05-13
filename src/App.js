f208276f2e29f2dff9c749b8e82bb8d9bb6d363c
salsailani <sf.sailani@gmail.com>
2021-05-13 18:15:46 -0400

    Added chart, navbar, buy/sell buttons components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {  Button, Navbar, Nav, FormControl, Form } from 'react-bootstrap';
+import TradingViewWidget, { Themes, BarStyles } from 'react-tradingview-widget';
+//import Login from './Login'
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory
+} from "react-router-dom";
+
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>
+      <Navbar2/>
+    </Router>
+    <CryptoName name="BTC"/>
+    <Chart ticker ="BINANCE:BTCUSD"/>
+    <BuyandSellButtons/>
     </div>
   );
 }
 
-export default App;
+
+
+function CryptoName(props){
+  return(
+    <div>
+
+    <text fontFamily='Kalam-Bold' style={{
+     fontSize: 40,
+     display: "flex",
+     justifyContent: "center",
+     alignItems: "center",
+     marginTop: '50px'
+    }}>
+
+    {props.name} </text>
+    </div>
+  );
+} 
+
+
+function Chart(props) {
+  return(
+  <div>
+  <TradingViewWidget
+    symbol={props.ticker}
+    theme={Themes.LIGHT}
+    locale="en"
+    style={BarStyles.LINE}
+  />
+  </div>
+  )
+}
+
+function BuyandSellButtons(){
+  return(
+    <div class="text-center">
+    <Button variant="outline-success m-3" size="lg">Buy</Button>
+    <Button variant="outline-danger m-3" size="lg">Sell</Button>
+    </div>
+  )
+}
+
+
+
+function Navbar2(){
+
+  let history = useHistory();
+   function routeChange(){ 
+    let path = './Login.js'; 
+    history.push(path);
+  }
+
+  return(
+  <div>
+  <Navbar bg="light" variant="light">
+    <Navbar.Brand href="#home">CRYPTO SIMULATOR</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">HOME</Nav.Link>
+      <Nav.Link href="#leaderboard">LEADERBOARD</Nav.Link>
+      <Nav.Link href="#FAQ">FAQ</Nav.Link>
+    </Nav>
+    <Form inline>
+      <Button  onClick={routeChange} variant="outline-info">Login/Signup</Button>
+    </Form>
+  </Navbar>
+</div>
+  )
+}
+
+
+function GetPrice(){
+  return(
+    <h1>.</h1>
+  )
+}
+
+
+
+
+export default App
